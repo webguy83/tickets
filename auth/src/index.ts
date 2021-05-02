3fcaa621144730f7cc7d5ac4c7a2fd9e4f3b94c5
Curtis <curtis_102@hotmail.com>
2021-05-02 00:03:33 -0700

    finished google cloud setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,10 @@ import { json } from 'body-parser';
 const app = express();
 app.use(json());
 
+app.get('/api/users/currentuser', (req, res) => {
+  res.send('You got me hahahahah')
+})
+
 app.listen(3000, () => {
   console.log('Listening on the port 3000 yayyy~ FUCKERRRRRR');
 });
