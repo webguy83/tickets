10306c0ab98cd43d0e6b56e5de97f8d01f0ad89b
Curtis <curtis_102@hotmail.com>
2021-05-11 01:14:29 -0700

    error middleware

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -9,9 +9,19 @@ import { signUpRouter } from './routes/signup';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found-error';
 import 'express-async-errors';
+import cookieSession from 'cookie-session';
 
 const app = express();
+app.set('trust proxy', true);
+
 app.use(json());
+
+app.use(
+  cookieSession({
+    secure: true,
+    signed: false,
+  })
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
@@ -26,6 +36,9 @@ app.use(errorHandler);
 
 const init = async () => {
   try {
+    if (!process.env.JWT_KEY) {
+      throw new Error('You need a JWT token!');
+    }
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth', {
       useNewUrlParser: true,
       useUnifiedTopology: true,
